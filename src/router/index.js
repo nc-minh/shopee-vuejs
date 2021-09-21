25f40f72c1f62788b5bba501b6ae0b7648bf1761
nguyencongminh-dev <nguyencongminh.official@gmail.com>
2021-09-21 17:09:07 +0700

    authod: ncm, config

--- src/router/index.js
+++ src/router/index.js
@@ -14,7 +14,13 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+  },
+  {
+    path: '/404',
+    name: '404',
+    component: () => import(/* webpackChunkName: "404" */ '../views/404'),
+  },
+  { path: '/:pathMatch(.*)*', redirect: '/404' }
 ]
 
 const router = createRouter({
---
+++ src/views/404.vue
@@ -0,0 +1,13 @@
+<template>
+  <h1>đây là trang bị lỗi</h1>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
