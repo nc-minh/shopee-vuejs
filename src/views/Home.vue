1544efdd8e710b78ce4cb8793e117e00fc96af45
nguyencongminh-dev <nguyencongminh.official@gmail.com>
2021-09-23 14:37:34 +0700

    authod: ncm, add: tailwindcss

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="home">
-    <h1>Heloo</h1>
+    <h1 class="text-purple-800">Heloo</h1>
     <i class="fa fa-cog" aria-hidden="true"></i>
   </div>
 </template>
---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+// tailwind.config.js
+module.exports = {
+    purge: [],
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+      extend: {
+        colors:{
+          dark: '#363853',
+          primary: '#0012FF',
+          green: '#67D4CA',
+          red: '#F00',
+          'bg': '#F8FAFB'
+        }
+      },
+    },
+    variants: {
+      extend: {},
+    },
+    plugins: [],
+  }
