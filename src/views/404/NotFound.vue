51ce05246253ec8658169d7d3fb7781edeabc56b
nc-minh <nguyencongminh.official@gmail.com>
2022-01-25 20:24:10 +0700

    :truck: update: router -> notFound page

---
+++ src/views/404/NotFound.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <img class="m-auto" src="https://cdn.tgdd.vn/hoi-dap/580732/loi-404-not-found-la-gi-9-cach-khac-phuc-loi-404-not-5-800x450.jpg" alt="">
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
